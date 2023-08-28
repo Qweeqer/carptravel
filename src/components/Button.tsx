401a3f9a04ed5dd21a98b47a43db86c3d366ed87
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-28 15:49:18 +0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-// import { ReactNode } from "react";
+import React, { ReactNode } from "react";
+
 import { Link } from "react-scroll";
 
-export default function Button({ children }: { children: React.ReactElement }) {
-
+export default function Button({ children }: { children: ReactNode }) {
   return (
     <Link
       className="btn relative w-full flex justify-center items-center text-[18px] lg:text-[32px] font-bold py-[14px] bg-white/5 duration-300 hover:bg-white/20 focus:bg-white/20 uppercase cursor-pointer"
