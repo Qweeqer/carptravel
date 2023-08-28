2a7fb8629bb1ae3064cbdf64fbb4993df1d6dd3d
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-28 15:59:39 +0300

    change styles

--- src/views/Gallery/Gallery.tsx
+++ src/views/Gallery/Gallery.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { useState} from "react";
+
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
@@ -21,8 +22,11 @@ export default function Gallery() {
         vertical: true,
         slides: { origin: "center", perView: 3, spacing: 24 },
       },
-    },
+      },
   });
+    
+
+
 
   return (
     <section
@@ -33,24 +37,26 @@ export default function Gallery() {
         <h2 className="title mb-[24px] md:mb-[66px] lg:mb-[15px] text-center lg:text-left">
           <span className="font-thin">{data.title.span1}</span>
           {data.title.span2}
-        </h2>
+              </h2>
+              
         <ul className="gallery_slider keen-slider h-full" ref={sliderRef}>
-          {data.images.map((image, idx) => (
-            <li className="keen-slider__slide" key={idx}>
-              <Image
-                className="w-full h-auto"
-                src={image.src}
-                alt={image.alt}
-                width={280}
-                height={187}
-                quality={100}
-              />
-            </li>
-          ))}
-        </ul>
+  {data.images.map((image, idx) => (
+    <li className="keen-slider__slide shadow-gallery" key={idx}>
+      <Image
+        className="w-full h-auto min-h-full"
+        src={image.src}
+        alt={image.alt}
+        width={280}
+        height={187}
+        quality={100}
+      />
+    </li>
+  ))}
+</ul>
+
 
         {loaded && instanceRef.current ? (
-          <div className="hidden md:flex md:gap-[459px] lg:gap-[651px] absolute top-[86%] left-[50%] transform -translate-x-1/2 lg:top-[78%]">
+          <div className="hidden md:flex md:gap-[230px] justify-around">
             <button
               className="text-[33px] font-thin leading-normal uppercase duration-300 hover:opacity-50 focus:opacity-50 border-b border-b-transparent hover:border-b-white focus:border-b-white"
               onClick={() => instanceRef.current?.prev()}
