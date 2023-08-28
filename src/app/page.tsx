994b5474c5fa0798a8ffc0fb2087d3fc64acc2a6
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-28 12:17:42 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Header from "@/views/Header";
-import Hero from "@/views/Hero";
-import About from "@/views/About";
-import Offers from "@/views/Offers";
-import Career from "@/views/Career";
-import Gallery from "@/views/Gallery";
-import Contacts from "@/views/Contacts";
+import Header from "@/views/Header/Header";
+import Hero from "@/views/Hero/Hero";
+import About from "@/views/About/About";
+import Offers from "@/views/Offers/Offers";
+import Career from "@/views/Career/Career";
+import Gallery from "@/views/Gallery/Gallery";
+import Contacts from "@/views/Contacts/Contacts";
 
 export default function Home() {
   return (
