67c3717d03551d192c08628765928462cc4725cb
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-29 22:07:24 +0300

    transfer to a separate Checkbox component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,8 +5,9 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import useFormPersist from "react-hook-form-persist";
 import toast, { Toaster } from "react-hot-toast";
 
-import { IFormData } from "@/types/interfaces";
+import { FormData } from "@/types/interfaces";
 import data from "@/data/form.json";
+import Checkbox from "./Checkbox";
 import InputError from "./InputError";
 
 export default function Form({ extended }: { extended: boolean }) {
@@ -21,9 +22,9 @@ export default function Form({ extended }: { extended: boolean }) {
     reset,
     setValue,
     formState: { errors },
-  } = useForm<IFormData>();
+  } = useForm<FormData>();
 
-  const onSubmit: SubmitHandler<IFormData> = (data) => {
+  const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(data);
     toast.success("Sent successfully!");
     setChecked(false);
@@ -172,35 +173,9 @@ export default function Form({ extended }: { extended: boolean }) {
           </label>
 
           <div className="relative md:flex md:justify-between">
-            {extended ? (
-              <div className="relative md:absolute md:top-0 md:-left-[242px] lg:-left-[314px] flex items-start gap-[8px] text-[12px] font-extralight leading-[22px] mb-[16px] lg:mb-[24px] group">
-                <div className="flex items-center justify-center w-[22px] h-[22px] lg:w-[24px] lg:h-[24px] border border-white">
-                  <div
-                    className={`${
-                      checked
-                        ? "bg-white"
-                        : "bg-white/10 duration-300 group-hover:bg-white/50 group-focus:bg-white/50"
-                    } w-[14px] h-[14px] lg:w-[16px] lg:h-[16px]`}
-                  >
-                    <input
-                      className="absolute top-0 bottom-0 left-0 right-0 cursor-pointer opacity-0"
-                      type="checkbox"
-                      onClick={() => setChecked(!checked)}
-                      {...register("checked", {
-                        required: data.errors.notEmpty,
-                      })}
-                    />
-                    {errors.checked ? (
-                      <InputError text={errors.checked?.message} />
-                    ) : null}
-                  </div>
-                </div>
-                <p className="inline-block md:w-[192px] lg:w-[258px]">
-                  {data.confirm}
-                </p>
-              </div>
-            ) : null}
-
+            {extended && (
+              <Checkbox {...{ register, errors, checked, setChecked }} />
+            )}
             <button
               className="block ml-auto text-[30px] lg:text-[32px] font-medium leading-normal uppercase duration-300 hover:opacity-60 focus:opacity-60 cursor-pointer"
               type="submit"
