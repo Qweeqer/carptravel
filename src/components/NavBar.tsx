4686999507dfe3c5982fc2d528b942810702082c
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-29 22:08:36 +0300

    refactoring code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,41 +7,43 @@ import Navigation from "./Navigation";
 
 export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { menu, close } = data;
 
-  const toggleMobileMenu = () => setMobileMenuOpen(!mobileMenuOpen);
-  const closeMobileMenu = () => setMobileMenuOpen(false);
+  const handleToggleMobileMenu = () => setMobileMenuOpen((prev) => !prev);
+  const handleCloseMobileMenu = () => setMobileMenuOpen(false);
 
   useEffect(() => {
-    mobileMenuOpen
-      ? (document.body.style.overflow = "hidden")
-      : (document.body.style.overflow = "auto");
+    document.body.style.overflow = mobileMenuOpen ? "hidden" : "auto";
   }, [mobileMenuOpen]);
+
+  const mobileMenuClasses = `
+    xs:absolute xs:z-20 xs:bg-bgMobile/75 xs:backdrop-blur-[25px]
+    ${
+      mobileMenuOpen
+        ? "xs:disable-scroll xs:fixed xs:left-0 xs:top-0 xs:w-screen xs:min-h-screen"
+        : "xs:static xs:hidden"
+    }
+  `;
 
   return (
     <div>
       <button
         className="uppercase md:hidden"
         type="button"
-        onClick={toggleMobileMenu}
+        onClick={handleToggleMobileMenu}
       >
-        {data.menu}
+        {menu}
       </button>
-      <div
-        className={`xs:absolute xs:z-20 xs:bg-bgMobile/75 xs:backdrop-blur-[25px] ${
-          mobileMenuOpen
-            ? "xs:disable-scroll xs:fixed xs:left-0 xs:top-0 xs:w-screen xs:min-h-screen"
-            : "xs:static xs:hidden"
-        }`}
-      >
+      <div className={mobileMenuClasses}>
         <div className="relative flex flex-col items-center justify-center xs:w-full xs:h-screen">
           <button
             className="uppercase absolute top-0 right-0 mt-[43px] mr-[20px] md:hidden"
             type="button"
-            onClick={closeMobileMenu}
+            onClick={handleCloseMobileMenu}
           >
-            {data.close}
+            {close}
           </button>
-          <Navigation onClick={closeMobileMenu} />
+          <Navigation onClick={handleCloseMobileMenu} />
         </div>
       </div>
     </div>
