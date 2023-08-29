818faad3868aedc7b94bc1120534742315b088df
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-29 22:08:03 +0300

    add passHref

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="w-[61px] h-[33px] duration-300 hover:opacity-60 focus:opacity-60 cursor-pointer">
-      <Link href="/" aria-label="CarpTravel logo">
+      <Link href="/" aria-label="CarpTravel logo" passHref>
         <Image src="/images/logo.svg" alt="Logo" width={61} height={33} />
       </Link>
     </div>
