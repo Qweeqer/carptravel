cf9848ab0a564336914b035ad97d7121cc49f95c
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-09-01 15:27:13 +0300

    Додано  метаданні Open Graph у компонент RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,24 @@
-import "../styles/globals.css";
 import { Inter } from "next/font/google";
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
+import "../styles/globals.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CarpTravel",
   description: "UNCOVER CARPATHIAN’S SECRETS",
+  openGraph: {
+    title: "CarpTravel - Uncover Carpathian’s Secrets",
+    description:
+      "Explore the beauty of the Carpathian Mountains and uncover their hidden secrets.",
+    url: "https://carptravel-one.vercel.app/",
+    images: [{ url: "/images/carptravel.png", width: 1200, height: 630 }],
+    type: "website",
+  },
 };
 
 function RootLayout({ children }: { children: React.ReactNode }) {
+  console.log("metadata", metadata);
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
